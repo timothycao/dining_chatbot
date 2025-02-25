f8789c4acc5a03d9f630ed0ea5373d6dbc1e2a30
timothycao <tcao.pro@gmail.com>
2025-02-25 13:55:41 -0500

    Fixed LF0 request and response format to align with frontend

--- lambda/lf0.py
+++ lambda/lf0.py
@@ -3,31 +3,54 @@ from lex.send import send_to_lex
 
 def lambda_handler(event, context):
     # Extract the user message from the API request
-    body = json.loads(event['body'])
-    user_message = body.get('message', '')
-    user_id = body.get('user_id', 'default')
+    user_id, user_message = 'default', ''
+
+    # Standard request format
+    if 'user_id' in event and 'message' in event:
+        user_id = event.get('user_id', 'default')
+        user_message = event.get('message', '')
+    
+    # Frontend request format
+    if 'messages' in event:
+        user_message = event['messages'][0]['unstructured']['text']
 
     # Send user message to Lex
     lex_response = send_to_lex(user_id, user_message)
 
     # Check if Lex responded successfully
     if lex_response and 'messages' in lex_response:
-        # Extract Lex's response message(s) as an array
-        response_message = [message['content'] for message in lex_response['messages']]
-        
+        # Frontend response format
+        response_messages = [
+            {
+                'type': 'unstructured',
+                'unstructured': {
+                    'text': message['content']
+                }
+            }
+            for message in lex_response['messages']
+        ]
+
         # Return Lex's response as API response
-        return {'statusCode': 200, 'body': json.dumps({'response': response_message})}
+        return {
+            'statusCode': 200,
+            'headers': {'Content-Type': 'application/json'},
+            'messages': response_messages
+        }
     else:
         # Return an error response if Lex didn't respond
-        return {'statusCode': 500, 'body': json.dumps({'error': 'Failed to get response from Lex'})}
+        return {'statusCode': 500, 'body': {'error': 'Failed to get response from Lex'}}
 
 # Testing
 if __name__ == '__main__':
     event = {
-        'body': json.dumps({
-            'userId': 'user-123',
-            'message': 'I need some restaurant suggestions'
-        })
+        'messages': [
+            {
+                'type': 'unstructured',
+                'unstructured': {
+                    'text': 'I need some restaurant suggestions'
+                }
+            }
+        ]
     }
     response = lambda_handler(event, None)
-    print('Response:', json.loads(response['body']))
+    print('Response:', response)
